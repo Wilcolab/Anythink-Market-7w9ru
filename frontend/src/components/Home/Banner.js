ba6a0b854df10794dcf7791ddb2ce18602fee559
WilcoApp <hello@wilco.gg>
2024-05-19 12:54:14 +0000

    Initial commit

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,22 +1,14 @@
 import React from "react";
 import logo from "../../imgs/logo.png";
 
-export const SearchBox = (props) => {
-  const changeHandler = (ev) => {
-    ev.preventDefault();
-    console.log(ev.target.value);
-  };
-  return <input id="search-box" onChange={changeHandler}></input>;
-};
-
 const Banner = () => {
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
-        <img src={logo} alt="banner" />
+        <img src={logo}/>
         <div>
-          <span id="get-part">A place to get</span>
-          <SearchBox></SearchBox>
+          <span>A place to </span>
+          <span id="get-part">get</span>
           <span> the cool stuff.</span>
         </div>
       </div>
